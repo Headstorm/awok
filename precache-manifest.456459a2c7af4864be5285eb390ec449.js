1da04a86ae5373d294a3931226646dbb7e64de82
dpianelli <dpianelli@users.noreply.github.com>
2020-06-01 17:33:20 +0000

    deploy: 660c6d0975e5e2bc282724d1d497968ce870f2e1

---
+++ precache-manifest.456459a2c7af4864be5285eb390ec449.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6c1e7e838a3ce399f4f6fd1360f8f9db",
+    "url": "/project-reckoning/index.html"
+  },
+  {
+    "revision": "2a27f9ec29d957d46026",
+    "url": "/project-reckoning/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0c7fd6b4e8093854868d",
+    "url": "/project-reckoning/static/js/2.86b6e1d5.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-reckoning/static/js/2.86b6e1d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a27f9ec29d957d46026",
+    "url": "/project-reckoning/static/js/main.3b6f9ae1.chunk.js"
+  },
+  {
+    "revision": "3e1e87d579d91102d6d3",
+    "url": "/project-reckoning/static/js/runtime-main.1dda87f2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/project-reckoning/static/media/logo.5d5d9eef.svg"
+  }
+]);
