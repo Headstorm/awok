8f0938061f9c8d9591d4d922f941d875abb5cb91
Enyi Asonye <enyiasonye@gmail.com>
2020-06-03 18:35:56 -0500

    change text alignment

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import styled from 'styled-components';
 import { withRouter } from 'react-router-dom';
 
 const Footer = () => {
   const BaseContainer = styled.div`
     padding: 1rem;
-    align-self: flex-end;
-    background-color: rgb(209, 224, 224,30%);
+    text-align: center;
+    background-color: rgb(209, 224, 224, 30%);
   `;
 
   return (
     <BaseContainer>
       All answers are completely anonymous. We do not track your phone,
-          location, or anything about you
+      location, or anything about you
     </BaseContainer>
-  )
+  );
 };
 
 export default withRouter(Footer);
