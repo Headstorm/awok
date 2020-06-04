2dae15e9f283ef1d622b7791d14b26208f710e46
dpianelli <dpianelli@users.noreply.github.com>
2020-06-04 19:36:37 +0000

    deploy: 1c0a65318c40e9c81db1f5bc49fff76d78db73c5

---
+++ precache-manifest.b019925ff6a5e49a002da96158457f07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "160a99f073906fd92612eac654482bc6",
+    "url": "/awok/index.html"
+  },
+  {
+    "revision": "88845a6e91c805423104",
+    "url": "/awok/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "fb3a3c619fe829d26ed9",
+    "url": "/awok/static/js/2.631a2dff.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/awok/static/js/2.631a2dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88845a6e91c805423104",
+    "url": "/awok/static/js/main.f1433b0a.chunk.js"
+  },
+  {
+    "revision": "3a3ae23bb4279142ecb6",
+    "url": "/awok/static/js/runtime-main.4977059d.js"
+  },
+  {
+    "revision": "872b2a80ebc543009c4dc659d4aadfce",
+    "url": "/awok/static/media/headstorm_logo.872b2a80.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/project-reckoning/precache-manifest.0e888e5381209a27b205f7a6f733142c.js"
+  "/awok/precache-manifest.b019925ff6a5e49a002da96158457f07.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/project-reckoning/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/awok/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
