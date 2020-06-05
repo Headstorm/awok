8126dd1c505bf4080d2acebc62096082c045975b
Enyi Asonye <enyiasonye@gmail.com>
2020-06-05 15:03:07 -0500

    add chart to admin page

--- src/screens/Admin.js
+++ src/screens/Admin.js
@@ -4,6 +4,7 @@ import MuiAlert from '@material-ui/lab/Alert';
 import styled from 'styled-components';
 import { withRouter } from 'react-router-dom';
 import { getSettings, setSettings } from '../apiCalls';
+import { Line } from 'react-chartjs-2';
 
 const BaseContainer = styled.div`
   display: flex;
@@ -24,6 +25,16 @@ const FooterDiv = styled.div`
   display: flex;
   justify-content: flex-end;
   align-items: flex-start;
+`;
+
+const MiddleDiv = styled.div`
+  display: flex;
+  justify-content: center;
+
+  canvas {
+    max-height: 40rem !important;
+    max-width: 40rem !important;
+  }
 `;
 
 const StyledButton = styled(Button)`
@@ -49,8 +60,35 @@ const Admin = (props) => {
   });
 
   const [open, setOpen] = useState(false);
-
+  // eslint-disable-next-line no-unused-vars
+  const [chartData, setChartData] = useState([0, 5, 16, 6, 8, 25, 0, 0]);
   const [savedSuccessfully, setSavedSuccessfully] = useState(true);
+
+  const lineChartInfo = {
+    labels: ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'],
+    datasets: [
+      {
+        label: 'Attendance',
+        fill: false,
+        lineTension: 0.5,
+        backgroundColor: '#288bea',
+        borderColor: '#288bea',
+        borderWidth: 3,
+        data: chartData,
+      },
+    ],
+  };
+
+  const lineChartOptions = {
+    title: {
+      display: true,
+      text: 'Attendance for Date - Date',
+      fontSize: 20,
+    },
+    legend: {
+      display: false,
+    },
+  };
 
   useEffect(() => {
     getSettings()
@@ -142,6 +180,9 @@ const Admin = (props) => {
           />
         </form>
       </AdminForm>
+      <MiddleDiv>
+        <Line data={lineChartInfo} options={lineChartOptions} />
+      </MiddleDiv>
       <FooterDiv>
         <StyledSnackBar
           autoHideDuration={3000}
