ab3494d8e32293abbe7c97b8f7ded5b8fa804739
Chloe Tedder <chloe_tedder@yahoo.com>
2020-06-09 15:09:35 -0500

    Add reservation screen

--- src/screens/Reservation.js
+++ src/screens/Reservation.js
@@ -1,64 +1,158 @@
 import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import ButtonGroup from '@material-ui/core/ButtonGroup';
-import { makeStyles } from '@material-ui/core/styles';
-import { TextField, withStyles } from '@material-ui/core';
+import { TextField, Divider, withStyles } from '@material-ui/core';
+import styled from "styled-components";
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    '& > *': {
-      margin: theme.spacing(2),
-    },
-  },
-}));
+const MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
+const BaseContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+`;
+
+const DaySelectionContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin: .5rem;
+`;
 
 const ReserveButton = withStyles(() => ({
-  root: {
-    color: '#FFFFFF',
-    backgroundColor: '#518DFD',
-  },
-}))(Button);
+		root: {
+			color: '#FFFFFF',
+			backgroundColor: '#518DFD',
+			width: '30%',
+			margin: '1rem',
+			"@media (max-width:425px)": { width: "75%" },
+		},
+	}))(Button);
+
+const StyledButtonGroup = withStyles(() => ({
+	root: {
+		margin: '.5rem .5rem 1rem .5rem',
+	}
+}))(ButtonGroup)
+
+const Form = styled.form`
+	width: 30%;
+	margin-top: 1rem;
+	@media (max-width:425px) {
+		width: 75%;
+	},
+`;
+
+const StyledTextField = withStyles(() => ({
+	root: {
+		width: '100%',
+	}
+}))(TextField)
+
+const StyledDivider = withStyles(() => ({
+		root: {
+			width: '35%',
+			margin: 0,
+			"@media (max-width:425px)": { width: "80%" },
+		}
+	}))(Divider)
+
+const SelectedDays = styled.p`
+	margin: .5rem;
+	width: 30%;
+	@media (max-width:425px) {
+		width: 80%;
+	},
+`;
+
+const StyledPressedButton = withStyles(() => ({
+	root: {
+		padding: '.5rem 1.5rem',
+		'background-color': '#518DFD',
+	}
+}))(Button)
+
+const StyledUnpressedButton = withStyles(() => ({
+	root: {
+		padding: '.5rem 1.5rem',
+		'background-color': '#FFFFFF'
+	}
+}))(Button)
 
 const Reservation = props => {
-	const classes = useStyles();
-	const [variant, setVariant] = useState(false);
+	const [storedDays, setStoredDays] = useState([]);
+	const [reservationCode, setReservationCode] = useState('');
+
+	const getSpecificDay = (numDay) => (new Date(new Date().setDate(new Date().getDate() - new Date().getDay() + numDay)).toISOString().slice(0, 10))
+
+	const getReadableDay = dateToConvert => {
+		const [month, numDay] = dateToConvert.slice(5).split('-')
+		return MONTHS[parseInt(month - 1)] + ' ' + numDay;
+	}
+
+	const today = new Date().toISOString().slice(0, 10)
+
+	const firstWeek = getReadableDay(getSpecificDay(1))
+	const firstWeekEnd = getReadableDay(getSpecificDay(5))
+	const secondWeek = getReadableDay(getSpecificDay(8))
+	const secondWeekEnd = getReadableDay(getSpecificDay(12))
+
+	const getButtons = startDay => {
+		return ['M','T','W','T','F'].map((d, x) => {
+			const storeValue = getSpecificDay(x + startDay)
+			const Button = storedDays.includes(storeValue) ? StyledPressedButton : StyledUnpressedButton;
+			return (
+				<Button
+					key={storeValue}
+					onClick={() => {
+						if (storedDays.includes(storeValue)) {
+							setStoredDays(storedDays.filter(day => day !== storeValue))
+						} else {
+							setStoredDays([...storedDays, storeValue])
+						}
+					}}
+					variant={storedDays.includes(storeValue) ? 'contained' : ''}
+					disabled={storeValue < today}
+				>
+					{d}
+				</Button>
+			)
+		})
+	}
+
   return (
-		<div className={classes.root}>
-			<label>Week of</label>
-		<ButtonGroup size="large" color="primary" aria-label="large outlined primary button group">
-			<Button onClick={() => setVariant(!variant)} variant={variant ? 'contained' : ''}>M</Button>
-			<Button>T</Button>
-			<Button>W</Button>
-			<Button>T</Button>
-			<Button>F</Button>
-    </ButtonGroup>
-		<label>Week of</label>
-		<ButtonGroup size="large" variant="contained" color="primary" aria-label="contained primary button">
-			<Button>M</Button>
-			<Button>T</Button>
-			<Button>W</Button>
-			<Button>T</Button>
-			<Button>F</Button>
-    </ButtonGroup>
-		<h4>Selected Days</h4>
-		<form noValidate>
-				<TextField
-				  id="outlined-basic"
-					label="Reservation Name"
-					variant="outlined"
-				/>
-		</form>
-		<ReserveButton
-			size="large"
-			variant="contained"
-			onClick={() => {}}
-		>
-      Reserve
-		</ReserveButton>
-		</div>
+		<BaseContainer>
+			<DaySelectionContainer>
+				<h2>Reserve your days</h2>
+				<label>Week of {firstWeek} - {firstWeekEnd}</label>
+				<StyledButtonGroup size="large" color="primary" aria-label="large outlined primary button">
+					{getButtons(1)}
+				</StyledButtonGroup>
+				<label>Week of {secondWeek} - {secondWeekEnd}</label>
+				<StyledButtonGroup size="large" color="primary" aria-label="large outlined primary button">
+					{getButtons(8)}
+				</StyledButtonGroup>
+			</DaySelectionContainer>
+			<SelectedDays><b>Selected Days</b></SelectedDays>
+			<StyledDivider variant="middle" />
+			<SelectedDays>{storedDays.sort().map(d => getReadableDay(d)).join(', ')}</SelectedDays>
+			<Form noValidate>
+					<StyledTextField
+						id="outlined-basic"
+						label="Reservation Retrival Code"
+						variant="outlined"
+						onChange={(e) => setReservationCode(e.target.value)}
+					/>
+			</Form>
+			<ReserveButton
+				disabled={!(storedDays.length && reservationCode.length)}
+				size="large"
+				variant="contained"
+				onClick={() => console.log(storedDays, reservationCode)}
+			>
+				Reserve
+			</ReserveButton>
+		</BaseContainer>
 	) 
 }
 
