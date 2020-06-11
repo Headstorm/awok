627219b796244985aceff3dd59d59dd21b9d9947
Dominique <dominique.d.pianelli@gmail.com>
2020-06-11 15:51:12 -0500

    update

--- src/common/Footer.js
+++ src/common/Footer.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
 import { withRouter } from 'react-router-dom';
+import { Typography, Link, Container } from '@material-ui/core';
 
 const BaseContainer = styled.div`
   padding: 1rem;
   text-align: center;
-  background-color: rgb(209, 224, 224, 30%);
-`;
+  background-color: #f0f2f2;
+  `;
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary">
+      {'Copyright © '}
+      <Link color="inherit" href="https://headstorm.com/">
+        Headstorm
+      </Link>{' '}
+      {new Date().getFullYear()}
+      {'.'}
+    </Typography>
+  );
+}
 
 const Footer = () => (
   <BaseContainer>
-    All answers are completely anonymous. We do not track your phone, location,
-    or anything about you
+    <Container maxWidth="sm">
+      <Typography variant="body1">
+        All answers are completely anonymous. We do not track your phone, location,
+        or anything about you.
+    </Typography>
+      <Copyright />
+    </Container>
   </BaseContainer>
 );
 
